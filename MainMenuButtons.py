6b910324b51b3522152a510d2e4fc855a71c528d
mkudm <mku.dmi@gmail.com>
2024-02-13 16:30:04 +0100

    add

---
+++ MainMenuButtons.py
@@ -0,0 +1,138 @@
+from telegram import *
+from telegram.ext import ContextTypes
+
+from messages.main_page import about_us_message, proposal_message, info_sport
+
+
+class AboutUsButton:
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        chat_id = query.message.chat_id
+        message_id = query.message.message_id
+
+        # Удаление предыдущего сообщения
+        await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+
+        photo_url = 'pics/main_menu/aboutus.jpg'
+        text = about_us_message
+        keyboard = [
+            [InlineKeyboardButton("Назад", callback_data='back')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_photo(chat_id=chat_id, photo=photo_url, caption=text, reply_markup=reply_markup)
+
+
+class InterestingPlacesButton:
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        chat_id = query.message.chat_id
+        message_id = query.message.message_id
+
+        await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+
+        photo_url = 'pics/main_menu/places.jpg'
+        text = "Интересные места"
+        keyboard = [
+            [InlineKeyboardButton("Парки", callback_data='parks')],
+            [InlineKeyboardButton("Музеи", callback_data='museums')],
+            [InlineKeyboardButton("Назад", callback_data='back')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_photo(chat_id=chat_id, photo=photo_url, caption=text, reply_markup=reply_markup)
+
+class SportsButton:
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        chat_id = query.message.chat_id
+        message_id = query.message.message_id
+
+        await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+
+        photo_url = 'pics/main_menu/sports.jpg'
+        text_to_send = info_sport
+        keyboard = [
+            [InlineKeyboardButton("Секции", callback_data='sections')],
+            [InlineKeyboardButton("Назад", callback_data='back')]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_photo(chat_id=chat_id, photo=photo_url, caption=text_to_send,
+                                     reply_markup=reply_markup)
+
+
+class SuggestionButton:
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        chat_id = query.message.chat_id
+        message_id = query.message.message_id
+
+        await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+
+        photo_url = 'pics/main_menu/proposal.jpg'
+        text_to_send = proposal_message
+        keyboard = [
+            [InlineKeyboardButton("Назад", callback_data='back')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_photo(chat_id=chat_id, photo=photo_url, caption=text_to_send,
+                                     reply_markup=reply_markup)
+
+
+class ShopsButton:
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        chat_id = query.message.chat_id
+        message_id = query.message.message_id
+
+        await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+
+        photo_url = 'pics/main_menu/shops.jpg'
+        text_to_send = 'Информация о магазинах'
+        keyboard = [
+            [InlineKeyboardButton("Книги", callback_data='4-1')],
+            [InlineKeyboardButton("Продукты", callback_data='4-2')],
+            [InlineKeyboardButton("Спорт", callback_data='4-3')],
+            [InlineKeyboardButton("Карнавал!", callback_data='4-4')],
+            [InlineKeyboardButton("Назад", callback_data='back')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_photo(chat_id=chat_id, photo=photo_url, caption=text_to_send,
+                                     reply_markup=reply_markup)
+
+class TransportButton:
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        chat_id = query.message.chat_id
+        message_id = query.message.message_id
+
+        await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+
+        photo_url = 'pics/main_menu/transport.jpg'
+        text_to_send = 'Транспорт'
+        keyboard = [
+            [InlineKeyboardButton("49 Euro Ticket", callback_data='5-1')],
+            [InlineKeyboardButton("Приложения", callback_data='5-2')],
+            [InlineKeyboardButton("Билеты", callback_data='5-3')],
+            [InlineKeyboardButton("За границу с проездным", callback_data='5-4')],
+            [InlineKeyboardButton("Назад", callback_data='back')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_photo(chat_id=chat_id, photo=photo_url, caption=text_to_send,
+                                     reply_markup=reply_markup)
+
+class StoriesButton:
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        chat_id = query.message.chat_id
+        message_id = query.message.message_id
+
+        await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+
+        photo_url = 'pics/main_menu/utilities.jpg'
+        text_to_send = 'Истории'
+        keyboard = [
+            [InlineKeyboardButton("Потеряли ключи от дома", callback_data='lost_keys')],
+            [InlineKeyboardButton("Назад", callback_data='back')],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_photo(chat_id=chat_id, photo=photo_url, caption=text_to_send,
+                                     reply_markup=reply_markup)
---
+++ main.py
@@ -0,0 +1,58 @@
+from telegram.ext import *
+
+from ButtonHandlers import ButtonHandlers
+from MainMenuButtons import *
+from messages.main_page import hello_message
+
+
+class Bot:
+    def __init__(self, token):
+        self.token = token
+        self.application = Application.builder().token(token).build()
+
+        self.button_handlers = ButtonHandlers()
+
+        self.application.add_handler(CommandHandler("start", self.start))
+        self.application.add_handler(CallbackQueryHandler(self.button))
+        self.application.add_handler(CommandHandler("help", self.help_command))
+
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        keyboard = [
+            [InlineKeyboardButton("Интересные места", callback_data='2')],
+            [InlineKeyboardButton("Магазины", callback_data='4')],
+            [InlineKeyboardButton("Спорт", callback_data='3')],
+            [InlineKeyboardButton("Транспорт", callback_data='6')],
+            [InlineKeyboardButton("Полезности", callback_data='7')],
+            [InlineKeyboardButton("О нас", callback_data='1'), InlineKeyboardButton("Предложка", callback_data='5')]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        message_text = hello_message
+        image_url = 'pics/main_menu/mainpage.jpg'
+        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=image_url, caption=message_text,
+                                     reply_markup=reply_markup)
+
+    async def button(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        await query.answer()
+
+        data = query.data
+
+        handler = self.button_handlers.get_handler(data)
+        if handler:
+            await handler.handle(update, context)
+        elif data == 'back':
+            chat_id = query.message.chat_id
+            message_id = query.message.message_id
+            await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+            await self.start(update, context)
+
+    async def help_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("Используйте /start для отображения кнопок")
+
+    def run(self):
+        self.application.run_polling()
+
+if __name__ == '__main__':
+    bot_token = "6951169119:AAH7q8B_YRa_sRk3nveqTzyIQVzb1bIqQMk"
+    bot = Bot(bot_token)
+    bot.run()
